ebd644472c2e0d8fe26e79e5d0e825ad49d64912
Mikkel-Student <61235342+Mikkel-Student@users.noreply.github.com>
2020-02-19 14:07:00 +0100

    Add files via upload

---
+++ Hand_Held_Shock_Tester/Hand_Held_Shock_Tester/main.c
@@ -0,0 +1,29 @@
+/*
+ * Hand_Held_Shock_Tester.c
+ *
+ * Created: 19-02-2020 13:58:15
+ * Author : mikke
+ */ 
+#define F_CPU 16000000UL
+
+
+//include standard libraries
+#include <avr/io.h>
+#include <stdio.h>
+#include <util/delay.h>
+#include "i2cmaster.h"
+#include "lcd.h"
+#include "usart.h"
+
+
+#define vref 4.8
+#define F_CPU 16000000UL
+
+
+int main(void)
+{
+    while (1) 
+    {
+    }
+}
+
